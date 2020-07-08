7dc493c34ebfe27359b0d3e0b03e01805554cfc9
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-08 23:47:17 +0530

    OrderedArray() method ends.

--- src/BinarySearching/OrderedArray/OrderedArray.java
+++ src/BinarySearching/OrderedArray/OrderedArray.java
@@ -8,11 +8,21 @@ package BinarySearching.OrderedArray;
 public class OrderedArray {
     /**
      * Some instance variables
+     * array to store data items
+     * nElems to store number of data items.
      */
     private long[] array;
     private int nElems;
     /**
      * Constructor to initialise default values to the instance variables.
+     * @param max the max (size of array)
      */
+
+    public OrderedArray(int max)
+    {
+        array = new long[max];
+        nElems = 0 ;
+    }
+
 
 }
