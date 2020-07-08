654a3876dc85c7e65b2794558507cdef40c7dede
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-08 23:43:53 +0530

    Parametrised Constructor

--- src/BinarySearching/OrderedArray/OrderedArray.java
+++ src/BinarySearching/OrderedArray/OrderedArray.java
@@ -1,5 +1,18 @@
 package BinarySearching.OrderedArray;
 
-
+/**
+ * Class declaration OrderedArray.java
+ * Performing Binary Search.
+ * This searching algorithm works on Sorted Data.
+ */
 public class OrderedArray {
+    /**
+     * Some instance variables
+     */
+    private long[] array;
+    private int nElems;
+    /**
+     * Constructor to initialise default values to the instance variables.
+     */
+
 }
