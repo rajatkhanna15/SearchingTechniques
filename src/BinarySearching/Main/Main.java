c1ef7d5fea0e7ee6ca86001ac675dac8e760fb6a
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-09 00:26:09 +0530

    main() method starts.

--- src/BinarySearching/Main/Main.java
+++ src/BinarySearching/Main/Main.java
@@ -1,4 +1,11 @@
 package BinarySearching.Main;
 
+/**
+ * This class acts as a tester class for OrderedArray.java class.
+ */
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
