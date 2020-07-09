aab5d9e4a0d52411d64c9a433aefd29914098751
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-09 11:32:42 +0530

    main() method starts.

--- src/LinearSearching/Main/Main.java
+++ src/LinearSearching/Main/Main.java
@@ -1,7 +1,9 @@
 package LinearSearching.Main;
 
 /**
- *
+ * Main class which is the tester class for SimpleArray class.
+ * Its not necessary for data to be in sorted order for performing Linear Search.
  */
 public class Main {
+
 }
