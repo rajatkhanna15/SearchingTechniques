5ba47ad082ca195b581a69cb7857b1c1746aa9a8
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-09 12:19:38 +0530

    SimpleArray.java class for showing the implementation of Linear Seaching.

--- src/LinearSearching/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package LinearSearching.Main;
-
-/**
- * Main class which is the tester class for SimpleArray class.
- * Its not necessary for data to be in sorted order for performing Linear Search.
- */
-public class Main {
-
-}
--- src/LinearSearching/SimpleArray/SimpleArray.java
+++ src/LinearSearching/SimpleArray/SimpleArray.java
@@ -1,45 +1,4 @@
 package LinearSearching.SimpleArray;
 
-/**
- * SimpleArray class for showing the basic implementation of linear searching.
- */
 public class SimpleArray {
-    /**
-     * Instance variables.
-     */
-    private long[] array;
-    private int nElems;
-
-    /**
-     * Parametrised constructor to initialise the values.
-     * @param max the max
-     */
-    public SimpleArray(int max)
-    {
-        array = new long[max];
-        nElems = 0 ;
-    }
-
-    /**
-     * size() method to return the current number of data items.
-     */
-    public int size()
-    {
-        return nElems;
-    }
-
-
-    /**
-     * find() method to show the basic implementation of linear searching.
-     * @param searchKey the searchKey.
-     * @return  the index where the element is found otherwise -1.
-     *
-     */
-    public int find(int searchKey)
-    {
-        for(int i = 0; i < array.length;i++)
-            if(array[i] == searchKey)
-                return i;
-            return -1;
-    }
 }
