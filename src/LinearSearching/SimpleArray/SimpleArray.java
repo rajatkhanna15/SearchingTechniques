2d1775ff5a67b0cddd0ca4287351a188023f24ac
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-09 12:25:19 +0530

    find() method ends.

--- src/LinearSearching/SimpleArray/SimpleArray.java
+++ src/LinearSearching/SimpleArray/SimpleArray.java
@@ -5,4 +5,18 @@ package LinearSearching.SimpleArray;
  */
 public class SimpleArray {
 
+    /**
+     * find() method to show the implementation of linear searching using an array and a searchkey
+     * @param inputArray the array
+     * @param searchKey the value to be searched
+     * @return index of searchkey if found else -1
+     */
+    public int find(long[] inputArray,int searchKey)
+    {
+        for (int i = 0; i < inputArray.length; i++) {
+            if(inputArray[i] == searchKey)
+                return i;
+        }
+        return -1;
+    }
 }
