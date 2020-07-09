e0d380ccf61befe65af0592cd571ec5db97ddd57
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-09 11:13:50 +0530

    SimpleArray() method starts.

--- src/LinearSearching/SimpleArray/SimpleArray.java
+++ src/LinearSearching/SimpleArray/SimpleArray.java
@@ -1,5 +1,13 @@
 package LinearSearching.SimpleArray;
 
-
+/**
+ * SimpleArray class for showing the basic implementation of linear searching.
+ */
 public class SimpleArray {
+    /**
+     * Instance variables.
+     */
+    private long[] array;
+    private int nElems;
+
 }
